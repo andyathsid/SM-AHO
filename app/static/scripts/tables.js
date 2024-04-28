a640703a2bc5e0aad1541e50c895d200fbe382bb
andyathsid <andakaraas@gmail.com>
2024-04-28 11:47:05 +0700

    Improve client-side data updating

--- app/static/scripts/tables.js
+++ app/static/scripts/tables.js
@@ -1,55 +1,52 @@
 const socket = io.connect('http://' + document.domain + ':' + location.port);
 
+socket.on('initial_data', function(data) {
+    createTable(data);
+});
+
 socket.on('stream_update', function(data) {
     updateTable(data);
 });
 
+function createTable(data) {
+    const tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    let index = 1;
+    Object.keys(data).forEach((key) => {
+        const rowData = data[key];
+        const row = document.createElement('tr');
+        const indexCell = document.createElement('td');
+        indexCell.textContent = index++;
+        row.appendChild(indexCell);
+        const idCell = document.createElement('td');
+        idCell.textContent = key;
+        row.appendChild(idCell);
+        const noMesinCell = document.createElement('td');
+        noMesinCell.textContent = rowData.no_mesin;
+        row.appendChild(noMesinCell);
+        const statusCell = document.createElement('td');
+        statusCell.textContent = rowData.status;
+        row.appendChild(statusCell);
+        const jumlahCell = document.createElement('td');
+        jumlahCell.textContent = rowData.jumlah;
+        row.appendChild(jumlahCell);
+        tableBody.appendChild(row);
+    });
+}
+
 function updateTable(data) {
     const tableBody = document.getElementById('table-body');
     const rows = tableBody.getElementsByTagName('tr');
-    const rowsData = {};
-    let index = 1; 
 
     for (let i = 0; i < rows.length; i++) {
         const cells = rows[i].getElementsByTagName('td');
         const id = cells[1].textContent;
-        rowsData[id] = {
-            no_mesin: cells[2].textContent,
-            status: cells[3].textContent,
-            jumlah: cells[4].textContent
-        };
-    }
-
-    tableBody.innerHTML = '';
-
-    Object.keys(data).forEach((key) => {
-        const rowData = data[key];
-        const row = document.createElement('tr');
-
-        const indexCell = document.createElement('td');
-        indexCell.textContent = index++;
-        row.appendChild(indexCell);
-
-        const idCell = document.createElement('td');
-        idCell.textContent = key;
-        row.appendChild(idCell);
-
-        const noMesinCell = document.createElement('td');
-        noMesinCell.textContent = rowData.no_mesin;
-        row.appendChild(noMesinCell);
-
-        const statusCell = document.createElement('td');
-        statusCell.textContent = rowData.status;
-        row.appendChild(statusCell);
-
-        const jumlahCell = document.createElement('td');
-        jumlahCell.textContent = rowData.jumlah;
-        row.appendChild(jumlahCell);
-
-        if (rowsData[key]) {
-            rowsData[key].jumlah = rowData.jumlah;
+        if (data[id]) {
+        const rowData = data[id];
+        cells[2].textContent = rowData.no_mesin;
+        cells[3].textContent = rowData.status;
+        cells[4].textContent = rowData.jumlah;
         }
-
-        tableBody.appendChild(row);
-    });
+    }
 }
